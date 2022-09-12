99d89492a8dd19b36d645c04cb7720fb5e477bf7
hrdy.adam <hrdy.adam@D20-16>
2022-09-12 13:08:51 +0200

    Add project files.

---
+++ clovece_nezlob_se/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace clovece_nezlob_se
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ clovece_nezlob_se/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clovece_nezlob_se
+{
+    internal class Class1
+    {
+    }
+
+    public class Dice
+    {
+        private Random random;
+        public int Value { get; set; }  
+
+        public Dice()
+        {
+            random = new Random();
+            Throw();
+        }
+
+        public void Throw()
+        {
+            Value = random.Next(1, 7);
+        }
+    }
+}
